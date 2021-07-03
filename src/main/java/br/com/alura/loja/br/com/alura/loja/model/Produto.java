f5e8154c1429435aadbe5a600bbeb9620b541187
michelle-santana <michelle.raiadrogasil@gmail.com>
2021-07-03 19:29:16 -0300

    Mapeando entidade

---
+++ src/main/java/br/com/alura/loja/br/com/alura/loja/model/Produto.java
@@ -0,0 +1,63 @@
+package br.com.alura.loja.br.com.alura.loja.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY )
+    @Column
+	private Long id;
+	
+    @Column
+	private String name;
+	
+    @Column
+	private String description;
+	
+    @Column
+	private BigDecimal price;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	
+
+}
